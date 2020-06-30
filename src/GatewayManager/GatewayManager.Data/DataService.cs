24c6707de35367c29d191211bf022c64a90f12bf
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-30 15:00:07 +0300

    Added DataService

---
+++ src/GatewayManager/GatewayManager.Data/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace GatewayManager.Data
+{
+    public class DataService<TEntity> : IDataService<TEntity> where TEntity : class
+    {
+        private readonly GatewayManagerDbContext _dbContext;
+
+        public DataService(GatewayManagerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IQueryable<TEntity> GetAll() => _dbContext.Set<TEntity>();
+
+        public async Task<TEntity> GetByIdAsync(object id) => await _dbContext.Set<TEntity>().FindAsync(id);
+
+        public void Update(TEntity entity) => _dbContext.Set<TEntity>().Update(entity);
+
+        public async Task Delete(object id)
+        {
+            var entity = await GetByIdAsync(id);
+
+            if (entity == null)
+            {
+                return;
+            }
+
+            _dbContext.Set<TEntity>().Remove(entity);
+        }
+
+        public async Task<int> SaveChangesAsync() => await _dbContext.SaveChangesAsync();
+    }
+}
---
+++ src/GatewayManager/GatewayManager.Data/IDataService.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace GatewayManager.Data
+{
+    public interface IDataService<TEntity> where TEntity : class
+    {
+        IQueryable<TEntity> GetAll();
+
+        Task<TEntity> GetByIdAsync(object id);
+
+        void Update(TEntity entity);
+
+        Task Delete(object id);
+
+        Task<int> SaveChangesAsync();
+    }
+}
