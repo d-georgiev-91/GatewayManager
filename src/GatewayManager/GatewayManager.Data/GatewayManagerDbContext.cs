1e80aa7abdd3bfa31f6288c8a3d91d15cd0bfe0c
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-30 12:45:53 +0300

    Added DbContext

---
+++ src/GatewayManager/GatewayManager.Data/GatewayManagerDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Humanizer;
+using Microsoft.EntityFrameworkCore;
+
+namespace GatewayManager.Data
+{
+    public class GatewayManagerDbContext : DbContext
+    {
+        public GatewayManagerDbContext(DbContextOptions<GatewayManagerDbContext> options) :
+            base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=GatewayManager;Trusted_Connection=True;MultipleActiveResultSets=true");
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
