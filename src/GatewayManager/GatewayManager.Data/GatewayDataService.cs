d36827d526dba6dc3e6723c48d869a63616cd894
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-02 19:36:23 +0300

    Added GatewayDataService

---
+++ src/GatewayManager/GatewayManager.Data/GatewayDataService.cs
@@ -0,0 +1,11 @@
+﻿using GatewayManager.DataModels;
+
+namespace GatewayManager.Data
+{
+    public class GatewayDataService : DataService<Gateway>
+    {
+        public GatewayDataService(GatewayManagerDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
