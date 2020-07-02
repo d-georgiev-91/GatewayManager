268000193c902e6b06ef9257acc8f709f7a18de8
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-02 19:40:30 +0300

    Added GatewayService

---
+++ src/GatewayManager/GatewayManager.Services/GatewayService.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using GatewayManager.Data;
+using GatewayManager.DataModels;
+
+namespace GatewayManager.Services
+{
+    public class GatewayService : IGatewayService
+    {
+        private readonly IDataService<Gateway> _gatewayDataService;
+
+        public GatewayService(IDataService<Gateway> gatewayDataService)
+        {
+            _gatewayDataService = gatewayDataService;
+        }
+
+        public async Task Add(Gateway gateway) => await _gatewayDataService.Add(gateway);
+    }
+}
---
+++ src/GatewayManager/GatewayManager.Services/IGatewayService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using GatewayManager.DataModels;
+
+namespace GatewayManager.Services
+{
+    public interface IGatewayService
+    {
+        Task Add(Gateway gateway);
+    }
+}
