3a403321983a30e301d67fee537f1dc15b4a22a3
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-02 20:03:22 +0300

    Added AutoMapper ApplicationProfile

---
+++ src/GatewayManager/GatewayManager.Web/AutoMapper/ApplicationProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using GatewayManager.DataModels;
+
+namespace GatewayManager.Web.AutoMapper
+{
+    public class ApplicationProfile : Profile
+    {
+        public ApplicationProfile()
+        {
+            CreateMap<Models.GatewayCreateModel, Gateway>();
+        }
+    }
+}
