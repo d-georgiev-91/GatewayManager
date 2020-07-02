2f893634baf480c3229af9b83564e1eeb3cb4313
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-02 20:02:39 +0300

    Added GatewayCreateModel

---
+++ src/GatewayManager/GatewayManager.Web/Models/GatewayCreateModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GatewayManager.Web.Models
+{
+    public class GatewayCreateModel
+    {
+        public string SerialNumber { get; set; }
+
+        public string Name { get; set; }
+
+        public string IPv4Address { get; set; }
+    }
+}
