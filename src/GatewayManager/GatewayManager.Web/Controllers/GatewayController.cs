773ca6ebb11c7e5dbc37e385cfd8cea066e3c702
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-02 20:03:42 +0300

    Added AutoMapper GatewayController

---
+++ src/GatewayManager/GatewayManager.Web/Controllers/GatewayController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using GatewayManager.DataModels;
+using GatewayManager.Services;
+using GatewayManager.Web.Models;
+
+namespace GatewayManager.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+
+    [ApiController]
+    [Route("[controller]")]
+    public class GatewayController : ControllerBase
+    {
+        private readonly IGatewayService _gatewayService;
+        private readonly IMapper _mapper;
+
+        public GatewayController(IGatewayService gatewayService, IMapper mapper)
+        {
+            _gatewayService = gatewayService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult Create(GatewayCreateModel gatewayCreateModel)
+        {
+            var gateway = _mapper.Map<GatewayCreateModel, Gateway>(gatewayCreateModel);
+            _gatewayService.Add(gateway);
+
+            return Ok();
+        }
+    }
+}
