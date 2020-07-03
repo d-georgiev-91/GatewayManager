98eb540e250e6de0a698905a5ccd0515f0164534
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-03 16:15:58 +0300

    Added GatewayDetails

---
+++ src/GatewayManager/GatewayManager.Web/Models/GatewayDetails.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace GatewayManager.Web.Models
+{
+    public class GatewayDetails
+    {
+        public string SerialNumber { get; set; }
+
+        public string Name { get; set; }
+
+        public string IPv4Address { get; set; }
+
+        public IReadOnlyCollection<PeripheralDevice> PeripheralDevices { get; set; }
+    }
+}
