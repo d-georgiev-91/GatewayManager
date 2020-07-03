146a3e9868099fcc110b24ef21a1f1db65ab2875
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-03 18:49:28 +0300

    Added AutoMapper TestsProfile

---
+++ src/GatewayManager/GatewayManager.Web.Tests/AutoMapper/TestsProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using GatewayManager.Web.Models;
+
+namespace GatewayManager.Web.Tests.AutoMapper
+{
+    public class TestsProfile : Profile
+    {
+        public TestsProfile()
+        {
+            CreateMap<GatewayCreateModel, DataModels.Gateway>();
+            CreateMap<DataModels.PeripheralDevice, PeripheralDevice>();
+            CreateMap<DataModels.Gateway, GatewayDetails>()
+                .ForMember(dest => dest.PeripheralDevices,
+                    opt => opt.MapFrom(g => g.PeripheralDevices));
+        }
+    }
+}
