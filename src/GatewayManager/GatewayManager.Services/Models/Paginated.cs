45ceb92e6a760951721988009245e3179e65e910
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-03 22:59:40 +0300

    Changed Data type to IEnumerable<TData>

---
+++ src/GatewayManager/GatewayManager.Services/Models/Paginated.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace GatewayManager.Services.Models
+{
+    public class Paginated<TData>
+    {
+        public IEnumerable<TData> Data { get; set; }
+
+        public int TotalCount { get; set; }
+    }
+}
