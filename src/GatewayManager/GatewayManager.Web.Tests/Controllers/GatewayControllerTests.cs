ce7f7230c48cda8a2dd2101ea5d1f69074a604b1
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-03 18:53:20 +0300

    Added tests for get of Gateway model

--- src/GatewayManager/GatewayManager.Web.Tests/Controllers/GatewayControllerTests.cs
+++ src/GatewayManager/GatewayManager.Web.Tests/Controllers/GatewayControllerTests.cs
@@ -1,9 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using AutoMapper;
-using GatewayManager.DataModels;
 using GatewayManager.Services;
 using GatewayManager.Web.Controllers;
 using GatewayManager.Web.Models;
+using GatewayManager.Web.Tests.AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
 using NUnit.Framework;
@@ -16,14 +18,23 @@ namespace GatewayManager.Web.Tests.Controllers
     {
         private IGatewayService _gatewayService;
         private GatewayController _gatewayController;
-        private IMapper _mapper;
+
+        private IMapper CreateAndConfigMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<TestsProfile>();
+            });
+
+            return config.CreateMapper();
+        }
 
         [SetUp]
         public void SetUp()
         {
             _gatewayService = Substitute.For<IGatewayService>();
-            _mapper = Substitute.For<IMapper>();
-            _gatewayController = new GatewayController(_gatewayService, _mapper);
+            var mapper = CreateAndConfigMapper();
+            _gatewayController = new GatewayController(_gatewayService, mapper);
         }
 
         [Test]
@@ -36,7 +47,7 @@ namespace GatewayManager.Web.Tests.Controllers
                 IPv4Address = "127.0.0.0"
             };
 
-            _gatewayService.AddAsync(Arg.Any<Gateway>())
+            _gatewayService.AddAsync(Arg.Any<DataModels.Gateway>())
                 .Returns(new ServiceResult());
 
             var actionResult = await _gatewayController.Create(gateway) as CreatedAtActionResult;
@@ -60,13 +71,57 @@ namespace GatewayManager.Web.Tests.Controllers
 
             var error = new ServiceResultError(ErrorType.InvalidInput, "Duplicate gateway");
 
-            _gatewayService.AddAsync(Arg.Any<Gateway>())
+            _gatewayService.AddAsync(Arg.Any<DataModels.Gateway>())
                 .Returns(new ServiceResult().AddError(error));
 
             var actionResult = await _gatewayController.Create(gateway) as BadRequestObjectResult;
 
             Assert.That(actionResult, Is.Not.Null);
             Assert.That(actionResult.Value, Is.EqualTo(error.Message));
+        }
+
+        [Test]
+        public async Task WhenGatewaySearchedBySerialNumberDoesNotExistThenNotFoundObjectResultShouldBeReturned()
+        {
+            const string serialNumber = "serial number";
+            var notFoundError = new ServiceResultError(ErrorType.NotFound, "Error");
+            var serviceResult = new ServiceResult<DataModels.Gateway>();
+            serviceResult.AddError(notFoundError);
+            _gatewayService.FindAsync(Arg.Any<string>()).Returns(serviceResult);
+            var result = await _gatewayController.GetDetails(serialNumber) as NotFoundObjectResult;
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Value, Is.EqualTo(notFoundError));
+        }
+
+        [Test]
+        public async Task WhenGatewaySearchedBySerialNumberDoesExistThenOkObjectResultShouldWithDataBeReturned()
+        {
+            const string serialNumber = "serial number";
+            var gateway = new DataModels.Gateway
+            {
+                SerialNumber = "Yd61wl8Voai3IFH",
+                Name = "Uber",
+                IPv4Address = "127.0.0.0",
+                PeripheralDevices = new List<DataModels.PeripheralDevice>
+                {
+                    new DataModels.PeripheralDevice
+                    {
+                        Uid = 1,
+                        DateCreated = DateTime.Now,
+                        IsOnline = true,
+                        Vendor = "Sony"
+                    }
+                }
+            };
+
+            var serviceResult = new ServiceResult<DataModels.Gateway> { Data = gateway };
+
+            _gatewayService.FindAsync(Arg.Any<string>()).Returns(serviceResult);
+            var result = await _gatewayController.GetDetails(serialNumber) as OkObjectResult;
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Value, Is.Not.Null);
         }
     }
 }
