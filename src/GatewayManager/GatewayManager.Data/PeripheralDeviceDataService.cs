30a7628b305d0519047a8a8075779d172ac1e094
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-03 23:45:27 +0300

    Added PeripheralDeviceDataService

---
+++ src/GatewayManager/GatewayManager.Data/PeripheralDeviceDataService.cs
@@ -0,0 +1,11 @@
+﻿using GatewayManager.DataModels;
+
+namespace GatewayManager.Data
+{
+    public class PeripheralDeviceDataService : DataService<PeripheralDevice>
+    {
+        public PeripheralDeviceDataService(GatewayManagerDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
