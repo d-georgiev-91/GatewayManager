a617609c9a497e7389ef614689d3340ed0141443
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-03 16:15:31 +0300

    Added PeripheralDevice model

---
+++ src/GatewayManager/GatewayManager.Web/Models/PeripheralDevice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GatewayManager.Web.Models
+{
+    public class PeripheralDevice
+    {
+        public long Uid { get; set; }
+
+        public string Vendor { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public bool IsOnline { get; set; }
+    }
+}
