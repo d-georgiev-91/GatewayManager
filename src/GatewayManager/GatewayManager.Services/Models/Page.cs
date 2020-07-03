d236410f861af754ba73ee60fb1f136c7585c6e4
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-03 22:44:51 +0300

    Added Paginated model to GatewayManager.Services

---
+++ src/GatewayManager/GatewayManager.Services/Models/Page.cs
@@ -0,0 +1,15 @@
+﻿namespace GatewayManager.Services.Models
+{
+    public class Page
+    {
+        /// <summary>
+        /// Page index zero based
+        /// </summary>
+        public int Index { get; set; }
+
+        /// <summary>
+        /// Page size
+        /// </summary>
+        public int Size { get; set; }
+    }
+}
