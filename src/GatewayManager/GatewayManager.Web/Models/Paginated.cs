16bcfd14f3d7dcd5c2a023afe5d722ece3528215
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-03 22:39:28 +0300

    Added Paginated model to GatewayManager.Web

---
+++ src/GatewayManager/GatewayManager.Web/Models/Paginated.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace GatewayManager.Web.Models
+{
+    public class Paginated<TData>
+    {
+        public IReadOnlyCollection<TData> Data { get; set; }
+
+        public int TotalCount { get; set; }
+    }
+}
