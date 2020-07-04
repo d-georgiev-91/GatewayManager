7424a2c817fc7246d3942e4b2f3530144bba17f1
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-04 23:21:33 +0300

    Added GatewayDeviceManager service

---
+++ src/GatewayManager/GatewayManager.Services/GatewayDeviceManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Tasks;
+
+namespace GatewayManager.Services
+{
+    public class GatewayDeviceManager : IGatewayDeviceManager
+    {
+        private const int AllowedPeripheralDevicesPerGateway = 10;
+
+        private readonly IGatewayService _gatewayService;
+        private readonly IPeripheralDeviceService _peripheralDeviceService;
+
+        public GatewayDeviceManager(IGatewayService gatewayService, IPeripheralDeviceService peripheralDeviceService)
+        {
+            _gatewayService = gatewayService;
+            _peripheralDeviceService = peripheralDeviceService;
+        }
+
+        public async Task<ServiceResult> AssignPeripheralDevice(string serialNumber, long peripheralDeviceId)
+        {
+            var gatewayServiceResult = await _gatewayService.FindAsync(serialNumber);
+
+            if (gatewayServiceResult.HasErrors)
+            {
+                return gatewayServiceResult;
+            }
+
+            var gateway = gatewayServiceResult.Data;
+
+            var serviceResult = new ServiceResult();
+
+            if (gateway.PeripheralDevices.Count >= AllowedPeripheralDevicesPerGateway)
+            {
+                serviceResult.AddError(ErrorType.InvalidInput,
+                    $"Only {AllowedPeripheralDevicesPerGateway} peripheral devices are allowed per gateway");
+
+                return serviceResult;
+            }
+
+            var peripheralDeviceServiceResult = await _peripheralDeviceService.GetByIdAsync(peripheralDeviceId);
+
+            if (peripheralDeviceServiceResult.HasErrors)
+            {
+                return peripheralDeviceServiceResult;
+            }
+
+            var peripheralDevice = peripheralDeviceServiceResult.Data;
+
+            _gatewayService.AddPeripheralDeviceAsync(gateway, peripheralDevice);
+
+            return serviceResult;
+        }
+    }
+}
---
+++ src/GatewayManager/GatewayManager.Services/IGatewayDeviceManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace GatewayManager.Services
+{
+    public interface IGatewayDeviceManager
+    {
+        Task<ServiceResult> AssignPeripheralDevice(string serialNumber, long peripheralDeviceId);
+    }
+}
