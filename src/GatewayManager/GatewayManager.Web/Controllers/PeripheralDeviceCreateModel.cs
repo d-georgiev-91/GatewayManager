0f0d2978a6c0d0f21b34b8af2a10a778d528d34c
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-04 13:08:27 +0300

    Added PeripheralDeviceCreateModel

---
+++ src/GatewayManager/GatewayManager.Web/Controllers/PeripheralDeviceCreateModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GatewayManager.Web.Controllers
+{
+    public class PeripheralDeviceCreateModel
+    {
+        public string Vendor { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public bool IsOnline { get; set; }
+    }
+}
