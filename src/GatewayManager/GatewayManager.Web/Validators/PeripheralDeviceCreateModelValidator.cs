f847193e87e5234cdb3e28f648fa4fe0b6f89466
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-04 13:26:54 +0300

    Added PeripheralDeviceCreateModelValidator

---
+++ src/GatewayManager/GatewayManager.Web/Validators/PeripheralDeviceCreateModelValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FluentValidation;
+using GatewayManager.Web.Models;
+
+namespace GatewayManager.Web.Validators
+{
+    public class PeripheralDeviceCreateModelValidator : AbstractValidator<PeripheralDeviceCreateModel>
+    {
+        public PeripheralDeviceCreateModelValidator()
+        {
+            RuleFor(peripheralDevice => peripheralDevice.Vendor).NotEmpty();
+            RuleFor(peripheralDevice => peripheralDevice.DateCreated).LessThanOrEqualTo(DateTime.Now);
+        }
+    }
+}
