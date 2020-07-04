2196587eb93550f2e74442ab7c9c478d4c098465
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-04 13:34:41 +0300

    Added PeripheralDeviceController

---
+++ src/GatewayManager/GatewayManager.Web/Controllers/PeripheralDeviceController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using GatewayManager.Services;
+using GatewayManager.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GatewayManager.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PeripheralDeviceController : BaseController
+    {
+        private readonly IPeripheralDeviceService _peripheralDeviceService;
+
+        public PeripheralDeviceController(IPeripheralDeviceService peripheralDeviceService, IMapper mapper) : 
+            base(mapper) => _peripheralDeviceService = peripheralDeviceService;
+
+        [HttpPost]
+        public async Task<IActionResult> Create(PeripheralDeviceCreateModel peripheralDeviceCreateModel)
+        {
+            var peripheralDevice = Mapper.Map<PeripheralDeviceCreateModel, DataModels.PeripheralDevice>(peripheralDeviceCreateModel);
+            await _peripheralDeviceService.AddAsync(peripheralDevice);
+
+            return Ok();
+        }
+    }
+}
