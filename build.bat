e955cc8a5ede9268af0213a22ea4ae7452afb3eb
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-07-06 00:24:42 +0300

    Added build script

---
+++ build.bat
@@ -0,0 +1,3 @@
+dotnet ef database update --project src/GatewayManager/GatewayManager.Web/GatewayManager.Web.csproj
+dotnet build src/GatewayManager
+dotnet run --project src/GatewayManager/GatewayManager.Web/GatewayManager.Web.csproj
